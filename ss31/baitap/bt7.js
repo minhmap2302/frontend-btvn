d76b0ff9d4bb035edf78efd3621f5023bb95cf5b
minhmap2302 <nguyentienminh.230206@gmail.com>
2025-03-20 22:48:46 +0700

    baitap

---
+++ ss31/baitap/bt7.js
@@ -0,0 +1,32 @@
+let todoList = [
+    {
+        id: 1,
+        task: 'Hit the gym',
+        completed: false,
+    },
+    {
+        id: 2,
+        task: 'Pay bills',
+        completed: true,
+    },
+    {
+        id: 3,
+        task: 'Meet george',
+        completed: false,
+    },
+    {
+        id: 4,
+        task: 'Buy eggs',
+        completed: false,
+    },
+    {
+        id: 5,
+        task: 'Read a book',
+        completed: false,
+    },
+    {
+        id: 6,
+        task: 'Organize office',
+        completed: false,
+    },
+];
